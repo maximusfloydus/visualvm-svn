dacf9238d46994bac620f7d3647f71e22bc2801a
nobody <nobody@ba0df5e5-9053-6d9f-941f-f112716aa91a>
2008-03-14 16:07:56 +0000

    This commit was manufactured by cvs2svn to create tag 'intel_demo_root'.

---
+++ tags/intel_demo_root/plugins/glassfish/src/net/java/visualvm/modules/glassfish/GlassFishApplicationTypeFactory.java
@@ -0,0 +1,79 @@
+/*
+/*
+ * Copyright 2007-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+
+package net.java.visualvm.modules.glassfish;
+
+import com.sun.tools.visualvm.core.datasource.Application;
+import com.sun.tools.visualvm.core.model.apptype.ApplicationType;
+import com.sun.tools.visualvm.core.model.apptype.ApplicationTypeFactory;
+import com.sun.tools.visualvm.core.model.apptype.MainClassApplicationTypeFactory;
+import com.sun.tools.visualvm.core.model.jmx.JmxModel;
+import com.sun.tools.visualvm.core.model.jmx.JmxModelFactory;
+import com.sun.tools.visualvm.core.model.jvm.JVM;
+import com.sun.tools.visualvm.core.model.jvm.JVMFactory;
+
+
+/**
+ *
+ * @author Jaroslav Bachorik
+ */
+public class GlassFishApplicationTypeFactory extends MainClassApplicationTypeFactory {
+    private final static GlassFishApplicationTypeFactory INSTANCE = new GlassFishApplicationTypeFactory();
+    
+    private GlassFishApplicationTypeFactory() {}
+    
+    public static void initialize() {
+        ApplicationTypeFactory.getDefault().registerFactory(INSTANCE);
+    }
+    
+    public static void shutdown() {
+        ApplicationTypeFactory.getDefault().unregisterFactory(INSTANCE);
+    }
+
+    @Override
+    public ApplicationType createModelFor(Application app) {
+        JVM jvm = JVMFactory.getJVMFor(app);
+        if (jvm.getMainClass() != null) return super.createModelFor(app);
+        if (jvm.isGetSystemPropertiesSupported()) {
+            if (jvm.getSystemProperties().get("com.sun.aas.instanceName") != null) {
+                return new GlassFishInstanceType(app, jvm);
+            }
+        }
+        return null;
+    }
+    
+    @Override
+    public ApplicationType createApplicationTypeFor(Application app, JVM jvm, String mainClass) {
+        if ("com.sun.enterprise.server.PELaunch".equals(mainClass)) {
+            return new GlassFishInstanceType(app, jvm);
+        } else if ("com.sun.enterprise.ee.nodeagent.NodeAgentMain".equals(mainClass)) {
+            return new GlassFishNodeType(jvm, app.getPid());
+        }
+
+        return super.createApplicationTypeFor(app, jvm, mainClass);
+    }
+}
